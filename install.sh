ffd9d1f0b10df22dca57edf13b76312b0d6ad459
Ben Neilsen <ben@neil.sn>
2025-09-27 00:59:36 -0400

    change conditional, make install.sh restart

--- install.sh
+++ install.sh
@@ -4,4 +4,5 @@ sudo cp listen-suspend /usr/local/bin/
 sudo cp listen-suspend.service /etc/systemd/user
 
 systemctl --user daemon-reload
-systemctl --user enable --now listen-suspend.service
+systemctl --user enable listen-suspend.service
+systemctl --user restart listen-suspend.service
